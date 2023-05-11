acc318b5595e649f0046e38aeb649279e577c44a
Rohit9113 <78945252+Rohit9113@users.noreply.github.com>
2023-05-11 22:52:26 +0530

    Drop Your Favorite Languages

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,29 +1,37 @@
+// Select all draggable elements
 const draggables = document.querySelectorAll(".draggable");
+
+// Select all dropzone elements
 const dropzones = document.querySelectorAll(".dropzone");
 
-// Add listeners for draggable elements
+// Add event listeners for draggable elements
 draggables.forEach(draggable => {
+    // When dragging starts, add "dragging" class to the element
     draggable.addEventListener("dragstart", () => {
         draggable.classList.add("dragging");
     });
 
+    // When dragging ends, remove "dragging" class from the element
     draggable.addEventListener("dragend", () => {
         draggable.classList.remove("dragging");
     });
 });
 
-// Add listeners for dropzone elements
+// Add event listeners for dropzone elements
 dropzones.forEach(dropzone => {
+    // When dragging over the dropzone, prevent the default action and add "dragover" class to the element
     dropzone.addEventListener("dragover", e => {
         e.preventDefault();
         dropzone.classList.add("dragover");
 
     });
 
+    // When dragging leaves the dropzone, remove the "dragover" class from the element
     dropzone.addEventListener("dragleave", () => {
         dropzone.classList.remove("dragover");
     });
 
+    // When dropping on the dropzone, prevent the default action and append the draggable element to the dropzone
     dropzone.addEventListener("drop", e => {
         e.preventDefault();
         const draggable = document.querySelector(".dragging");
@@ -33,5 +41,3 @@ dropzones.forEach(dropzone => {
         dropzone.classList.remove("dragover");
     });
 });
-
-
